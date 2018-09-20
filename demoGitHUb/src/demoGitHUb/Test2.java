fe6540ab6011dd37458f1c8d2c7e2d494e16ea74
ubalija <31179388+ubalija@users.noreply.github.com>
2018-09-20 19:08:48 +0530

    Added code for Test Case 002
    Change-Id: Ie2b77fee423fcf09f6c36be820f2e50aa4ccd3cf
    Signed-off-by: ubalija <31179388+ubalija@users.noreply.github.com>

---
+++ demoGitHUb/src/demoGitHUb/Test2.java
@@ -0,0 +1,12 @@
+package demoGitHUb;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Added new method for TC002");
+
+	}
+
+}
